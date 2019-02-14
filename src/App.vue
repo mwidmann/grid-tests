9b380dcd66967ce89be09a435b942d38f283a44a
mwidmann <widmann.martin@gmail.com>
2019-02-14 12:43:31 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app"><ItemsList /></div>
+</template>
+
+<script>
+import ItemsList from "./components/ItemsList";
+
+export default {
+  name: "App",
+  components: {
+    ItemsList
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/ItemsList.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <div class="grid" :style="{ 'grid-template-areas': gridAreas }">
+      <div
+        v-for="(item, index) in items"
+        :key="index"
+        class="grid__item"
+        :style="{
+          'background-color': randomColor(),
+          'grid-area': `a${index}`
+        }"
+      >
+        {{ `a${index}` }}
+      </div>
+    </div>
+    <div><button @click="moarItems">➕ Moar</button></div>
+  </div>
+</template>
+
+<script>
+const layouts = [
+  {
+    className: "layout-1",
+    items: 3,
+    template: [[0, 0, 0, 0, 1, 1], [0, 0, 0, 0, 2, 2]]
+  },
+  { className: "layout-2", items: 3, template: [[0, 0, 1, 1, 2, 2]] },
+  { className: "layout-3", items: 1, template: [[0, 0, 0, 0, 0, 0]] },
+  {
+    className: "layout-4",
+    items: 3,
+    template: [[1, 1, 0, 0, 0, 0], [2, 2, 0, 0, 0, 0]]
+  },
+  { className: "layout-5", items: 3, template: [[0, 0, 1, 1, 2, 2]] },
+  { className: "layout-6", items: 2, template: [[0, 0, 0, 1, 1, 1]] }
+];
+
+export default {
+  data() {
+    return {
+      items: [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}]
+    };
+  },
+  computed: {
+    gridAreas() {
+      let currentLayout = 0;
+      let grid = "";
+      for (let index = 0; index <= this.items.length; index++) {
+        const layout = layouts[currentLayout];
+        grid = `
+${grid}
+/* ${layout.className} */
+${layout.template
+          .map(line => `"${line.map(e => `a${index + e}`).join(" ")}"`)
+          .join("\n")}
+`;
+
+        index += layout.items - 1;
+        currentLayout++;
+        if (currentLayout > layouts.length - 1) currentLayout = 1;
+      }
+      return grid;
+    }
+  },
+  methods: {
+    randomColor() {
+      var letters = "0123456789ABCDEF";
+      var color = "#";
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    },
+    moarItems() {
+      this.items = [...this.items, ...this.items.splice(0, 20)];
+      console.log(this.items.length);
+    }
+  }
+};
+</script>
+
+<style>
+pre {
+  text-align: left;
+}
+.grid {
+  display: grid;
+  grid-template-columns: repeat(6, 1fr);
+  grid-gap: 1rem;
+}
+.grid__item {
+  color: white;
+  font-size: 4rem;
+  font-weight: bold;
+  text-shadow: 2px 2px 2px rgba(0, 0, 0, 0.4);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 10rem;
+}
+</style>
